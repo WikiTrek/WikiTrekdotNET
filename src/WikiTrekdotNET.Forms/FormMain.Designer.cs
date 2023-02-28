1b31975853abc86ed39eee97eeebb0cde2473c05
Luca Mauri <lucamauricom@hotmail.com>
2023-02-28 18:11:57 +0100

    Basic tests with WikiClientLibrary

--- src/WikiTrekdotNET.Forms/FormMain.Designer.cs
+++ src/WikiTrekdotNET.Forms/FormMain.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BTNTest = new System.Windows.Forms.Button();
+            this.TXTUser = new System.Windows.Forms.TextBox();
+            this.TXTPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BTNTest
+            // 
+            this.BTNTest.Location = new System.Drawing.Point(857, 588);
+            this.BTNTest.Name = "BTNTest";
+            this.BTNTest.Size = new System.Drawing.Size(75, 23);
+            this.BTNTest.TabIndex = 0;
+            this.BTNTest.Text = "Test";
+            this.BTNTest.UseVisualStyleBackColor = true;
+            this.BTNTest.Click += new System.EventHandler(this.BTNTest_Click);
+            // 
+            // TXTUser
+            // 
+            this.TXTUser.Location = new System.Drawing.Point(832, 536);
+            this.TXTUser.Name = "TXTUser";
+            this.TXTUser.Size = new System.Drawing.Size(100, 20);
+            this.TXTUser.TabIndex = 1;
+            // 
+            // TXTPassword
+            // 
+            this.TXTPassword.Location = new System.Drawing.Point(832, 562);
+            this.TXTPassword.Name = "TXTPassword";
+            this.TXTPassword.PasswordChar = '*';
+            this.TXTPassword.Size = new System.Drawing.Size(100, 20);
+            this.TXTPassword.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.ClientSize = new System.Drawing.Size(944, 707);
+            this.Controls.Add(this.TXTPassword);
+            this.Controls.Add(this.TXTUser);
+            this.Controls.Add(this.BTNTest);
+            this.Name = "FormMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+
         #endregion
+
+        private System.Windows.Forms.Button BTNTest;
+        private System.Windows.Forms.TextBox TXTUser;
+        private System.Windows.Forms.TextBox TXTPassword;
     }
 }
 
--- src/WikiTrekdotNET.Forms/FormMain.cs
+++ src/WikiTrekdotNET.Forms/FormMain.cs
@@ -4,9 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WikiClientLibrary;
+using WikiClientLibrary.Client;
+using WikiClientLibrary.Sites;
 
 namespace WikiTrekdotNET.Forms
 {
@@ -15,6 +19,89 @@ namespace WikiTrekdotNET.Forms
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        static async Task MainAsync(string UserName, string Password)
+        { 
+            WikiClient GeneralClient = new WikiClient
+            {
+                ClientUserAgent = "WCLQuickStart/1.0 (your user name or contact information here)"
+            };
+
+            // WikiSite WikiTrek = new WikiSite(GeneralClient, "https://wikitrek.org/wt/api.php");
+            WikiSite DataTrek = new WikiSite(GeneralClient, "https://data.wikitrek.org/dt/api.php");
+
+            Console.WriteLine("Initialization");
+            await DataTrek.Initialization;
+            Console.WriteLine("Initialized");
+
+            try
+            {
+                await DataTrek.LoginAsync(UserName, Password);
+                
+            }
+            catch (WikiClientException ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Add your exception handler for failed login attempt.
+            }
+                        
+            try
+            {
+                // Do what you want
+                Console.WriteLine(DataTrek.SiteInfo.SiteName);
+                Console.WriteLine(DataTrek.AccountInfo);
+                Console.WriteLine("{0} extensions", DataTrek.Extensions.Count);
+                Console.WriteLine("{0} interwikis", DataTrek.InterwikiMap.Count);
+                Console.WriteLine("{0} namespaces", DataTrek.Namespaces.Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // We're done here
+            await DataTrek.LogoutAsync();
+            GeneralClient.Dispose();
+        }
+
+        private void BTNTest_Click(object sender, EventArgs e)
+        {
+            MainAsync(TXTUser.Text, TXTPassword.Text).Wait();
+            //MainSync(TXTUser.Text, TXTPassword.Text);
+        }
+
+        static void MainSync(string UserName, string Password)
+        {
+            WikiClient GeneralClient = new WikiClient();
+
+            // WikiSite WikiTrek = new WikiSite(GeneralClient, "https://wikitrek.org/wt/api.php");
+            WikiSite DataTrek = new WikiSite(GeneralClient, "https://data.wikitrek.org/dt/api.php");
+
+            Console.WriteLine("Initialization");
+            //DataTrek.Initialization;
+            Console.WriteLine("Initialized");
+
+            try
+            {
+                DataTrek.LoginAsync(UserName, Password);
+
+                // Do what you want
+                Console.WriteLine(DataTrek.SiteInfo.SiteName);
+                Console.WriteLine(DataTrek.AccountInfo);
+                Console.WriteLine("{0} extensions", DataTrek.Extensions.Count);
+                Console.WriteLine("{0} interwikis", DataTrek.InterwikiMap.Count);
+                Console.WriteLine("{0} namespaces", DataTrek.Namespaces.Count);
+            }
+            catch (WikiClientException ex)
+            {
+                Console.WriteLine(ex.Message);
+                // Add your exception handler for failed login attempt.
+            }
+
+            // We're done here
+            DataTrek.LogoutAsync();
+            GeneralClient.Dispose();
         }
     }
 }
